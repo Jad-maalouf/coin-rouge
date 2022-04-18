f7396106c5f519c99abab6e2b2606b181d133e00
Jad Maalouf <80184480+Jad-maalouf@users.noreply.github.com>
2022-04-18 04:04:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+let button = document.getElementById('alcohol-btn');
+
+
+
+
+let menu = document.querySelector('.alcohol');
+var test = true;
+function hidemenu(){
+if (test){
+    menu.style.display = "block";
+    test = false;
+}else{
+    menu.style.display = "none";
+    test = true;
+}
+}
+
+
+let menuu = document.querySelector('.food');
+var testt = true;
+function hidemenuu(){
+if (testt){
+    menuu.style.display = "block";
+    testt = false;
+}else{
+    menuu.style.display = "none";
+    testt = true;
+}
+}
+
+
+let menuuu = document.querySelector('.beverage');
+var testtt = true;
+function hidemenuuu(){
+if (testtt){
+    menuuu.style.display = "block";
+    testtt = false;
+}else{
+    menuuu.style.display = "none";
+    testtt = true;
+}
+}
